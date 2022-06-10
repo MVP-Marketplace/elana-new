71cad0ee4bd8dfea6466e8be537ec676c76e3223
lucciii33 <angelomaiele@gmail.com>
2022-06-10 01:14:20 +0000

    logo responsives

--- frontend/src/pages/Practitioner/PractitionerLogin.jsx
+++ frontend/src/pages/Practitioner/PractitionerLogin.jsx
@@ -6,6 +6,7 @@ import { login, reset } from '../../features/auth/authSlice'
 import Spinner from '../../components/spinner'
 import { MDBInput } from 'mdb-react-ui-kit';
 import Logo from "../../img/LogoName.png";
+import Logores from "../../img/logoresponsive.png";
 import '../../index.css'
 
 export function PractitionerLogin() {
@@ -65,13 +66,22 @@ export function PractitionerLogin() {
                 {/* /////// first part logo and icon*/}
                 <div className="p-3">
                     <img src={Logo} className="logosingup" alt='Elana Logo'></img>
+                    
                 </div>
 
                 {/* /////// second part titles*/}
 
-                <div className="text-center">
+                <div className="">
+                    <div className='d-flex'>
+                    <div className='responsivediv'>
+                    <img src={Logores} className="Logores" alt='Elana Logo'></img>
+                    </div>
+                    <div className='text-center'>
                     <h2 className="titlesingup text-center">Practitioner Log In</h2>
-                    <p className="labelsingup">Don`t have an account? Click <span>here</span> to Sign Up</p>
+                    <p className="labelsingup text-center">Don`t have an account? Click <span>here</span> to Sign Up</p>
+                    </div>
+                    </div>
+
                 </div>
 
                 {/* /////// third part inputs*/}
@@ -99,7 +109,7 @@ export function PractitionerLogin() {
                             onChange={onChange} />
                     </div>
 
-                    <button className='buttonmainpage col-sm m-5' onClick={onSubmit}>
+                    <button className='buttonmainpage col-sm m-4' onClick={onSubmit}>
                         LOG IN
                     </button>
                     </div>
